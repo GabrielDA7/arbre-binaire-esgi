4728bbc6700e9f1e6f4bed57b39e41a96414ef54
Gabriel Daoud <gabriel.pro.d3@gmail.com>
2018-06-01 21:12:04 +0200

    fix class

--- AB.py
+++ AB.py
@@ -1,3 +1,4 @@
+
 # coding=utf-8
 class Noeud:
     """ Constructeur de la classe Noeud """
@@ -14,31 +15,25 @@ class Noeud:
     def FilsGauche(self):
         if(self.estVide()):
             return self.valeur
-        else:
-            return self.gauche
+        return self.gauche
 
     """ Retourne la valeur du fils droite """
     def FilsDroite(self):
         if(self.estVide()):
             return self.valeur
-        else:
-            return self.droite
+        return self.droite
 
     """ Retourne un boolean determinant si le noeud n'a aucun fils """
     def estFeuille(self):
-        if(self.estVide()):
-            return False
-        elif((self.FilsGauche() == None) and (self.FilsDroite() == None)):
+        if((self.FilsGauche() == None) and (self.FilsDroite() == None)):
             return True
-        else:
-            return False
+        return False
 
     """ Retourne un boolean determinant si le noeud a au moins 1 fils """
     def estNoeudInterne(self):
         if(self.estFeuille()):
             return False
-        else:
-            return True
+        return True
 
     """ Retourne un boolean determinant si le noeud a deux fils """
     def estDouble(self):
@@ -48,92 +43,123 @@ class Noeud:
     def estSimple(self):
         if((self.gauche is None) and (self.droite is not None)):
             return True
-        elif((self.gauche is not None) and (self.droite is None)):
+        if((self.gauche is not None) and (self.droite is None)):
             return True
-        else:
-            return False
+        return False
 
 
     """ Permet de construire l'abre """
     @staticmethod
     def creer(A=None):
-            valeur = input("Entrez la valeur du noeud : ")
-            A = Noeud(valeur)
-            gauche = ""
-            droite = ""
-            while((gauche != "oui") and (gauche != "non")):
-                gauche = raw_input("Creer un noeud à gauche ? (oui/non) : \n")
-            if(gauche == "oui"):
-                A.gauche = Noeud.creer(A.gauche)
-            while((droite != "oui") and (droite != "non")):
-                droite = raw_input("Creer un noeud à droite ? (oui/non) : \n")
-            if(droite == "oui"):
-                A.droite = Noeud.creer(A.droite)
+            A = Noeud()
+            valeur = raw_input("Entrez la valeur du noeud : ")
+            if(valeur != None and valeur != ""):
+                A = Noeud(valeur)
+                gauche = ""
+                droite = ""
+                while((gauche != "oui") and (gauche != "non")):
+                    gauche = raw_input("Creer un noeud à gauche ? (oui/non) : \n")
+                if(gauche == "oui"):
+                    A.gauche = Noeud.creer(A.gauche)
+                while((droite != "oui") and (droite != "non")):
+                    droite = raw_input("Creer un noeud à droite ? (oui/non) : \n")
+                if(droite == "oui"):
+                    A.droite = Noeud.creer(A.droite)
             return A
 
     """ Permet d'afficher l'arbre """
     def afficher(self):
     		if(self.estDouble()):
     			return str(self.valeur)+"("+self.gauche.afficher()+";"+self.droite.afficher()+")"
-    		elif(self.gauche != None):
+    		if(self.gauche != None):
     			return str(self.valeur)+"("+self.gauche.afficher()+";)"
-    		elif(self.droite != None):
+    		if(self.droite != None):
     			return str(self.valeur)+"(;"+self.droite.afficher()+")"
-    		else:
-    			return str(self.valeur)
+    		return str(self.valeur)
 
-""" Retourne la taille de l'abre """
-def taille(arbre):
-    if(arbre == None):
-    	return 0
-    else:
-    	return 1 + taille(arbre.gauche) + taille(arbre.droite)
+    """ Retourne la taille de l'abre """
+    def taille(self):
+        if(self.valeur == None):
+        	return 0
+        if(self.FilsGauche() != None):
+            tailleGauche = self.FilsGauche().taille()
+        else:
+            tailleGauche = 0
+        if(self.FilsDroite() != None):
+            tailleDroite = self.FilsDroite().taille()
+        else:
+            tailleDroite = 0
+    	return 1 + tailleGauche + tailleDroite
 
-""" Retourne la hauteur de l'abre """
-def hauteur(arbre):
-    if(arbre == None):
-        return 0
-    else:
-        return 1 + max(hauteur(arbre.gauche),hauteur(arbre.droite))
+    """ Retourne la hauteur de l'abre """
+    def hauteur(self):
+        if(self.estVide()):
+            return -1
+        if(arbre.estFeuille()):
+            return 0
+        if(self.FilsGauche() != None):
+            hauteurGauche = self.FilsGauche().hauteur()
+        else:
+            hauteurGauche = -1
+        if(self.FilsDroite() != None):
+            hauteurDroite = self.FilsDroite().hauteur()
+        else:
+            hauteurDroite = -1
+        return 1 + max(hauteurGauche,hauteurDroite)
 
-""" Retourne un boolean determinant si l'abre est équilibré """
-def estEquilibre(arbre):
-    if(arbre == None):
-        return True
-    tailleGauche = hauteur(arbre.gauche)
-    tailleDroite = hauteur(arbre.droite)
-    if(abs(tailleGauche - tailleDroite) <= 1 and estEquilibre(arbre.gauche) and estEquilibre(arbre.droite)):
-        return True
-    return False;
+    """ Retourne un boolean determinant si l'abre est équilibré """
+    def estEquilibre(self):
+        if(self.estVide()):
+            return True
+        if(self.FilsGauche() != None):
+            hauteurGauche = self.FilsGauche().hauteur()
+            equilibreG = self.FilsGauche().estEquilibre()
+        else:
+            hauteurGauche = 0
+            equilibreG = True
+        if(self.FilsDroite() != None):
+            hauteurDroite = self.FilsDroite().hauteur()
+            equilibreD = self.FilsDroite().estEquilibre()
+        else:
+            hauteurDroite = 0
+            equilibreD = True
+        if(abs(hauteurGauche - hauteurDroite) <= 1 and equilibreG and equilibreD):
+            return True
+        return False;
 
-""" Retourne les valeurs des noeuds d'un arbre dans l'ordre du parcour prefix (r, g, d)"""
-def parcourPrefix(arbre):
-    if (arbre != None):
+    """ Retourne les valeurs des noeuds d'un arbre dans l'ordre du parcour prefix (r, g, d)"""
+    def parcourPrefix(self):
+        if (self.estVide()):
+            return None
         print arbre.valeur
-        parcourPrefix(arbre.gauche)
-        parcourPrefix(arbre.droite)
-    else:
-        return None
+        if(self.FilsGauche() != None):
+            self.FilsGauche().parcourPrefix()
+        if(self.FilsDroite() != None):
+            self.FilsDroite().parcourPrefix()
 
-""" Rotation gauche de l'arbre donné en paramètre ( destructive method ) """
-def rotationGauche(arbre):
-    arbreBis = arbre.droite
-    arbre.droite = arbreBis.gauche
-    arbreBis.gauche = arbre
-    return arbreBis
+    """ Rotation gauche de l'arbre donné en paramètre ( destructive method ) """
+    def rotationGauche(self):
+        if(self.FilsDroite() == None):
+            return self
+        arbreBis = self.FilsDroite()
+        arbre.droite = arbreBis.FilsGauche()
+        arbreBis.gauche = arbre
+        return arbreBis
+
 
 arbre = Noeud.creer()
+
 print("Arbre :  " + arbre.afficher())
 print("Parcour Prefix : ")
-parcourPrefix(arbre)
+arbre.parcourPrefix()
 print("L'abre est-il vide ? ")
 print(arbre.estVide())
 print("Taille de l'abre : ")
-print(taille(arbre))
+print(arbre.taille())
 print("Hauteur de l'abre : ")
-print(hauteur(arbre))
+print(arbre.hauteur())
 print("L'abre est-il équilibré ? ")
-print(estEquilibre(arbre))
+print(arbre.estEquilibre())
 print("Rotation gauche de l'abre : ")
-arbre = rotationGauche(arbre)
+arbre = arbre.rotationGauche()
 print("Arbre :  " + arbre.afficher())
